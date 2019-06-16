839038f3bd7f5c75a1c2d8c53a3b488174f2d72d
arcadia168 <bennawaz168@icloud.com>
2019-06-16 20:56:33 +0100

    Adding prop types to repo and setting up

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types'
 
 const HelloWorld = () => <h1 data-testid="app-title" className="app__title">Hello World</h1>;
 
@@ -11,6 +12,10 @@ const App = () => {
       <Button>Hello React Start with Bootstrap!</Button>
     </Router>
   )
+}
+
+App.propTypes = {
+  axiosInstance: PropTypes.object,
 }
 
 export default App;
