e20119dc378f4ca3c06a54b935447a514e305ad3
Erin Eland <erin@erineland.com>
2019-06-16 20:56:33 +0100

    Adding prop types to repo and setting up

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,9 @@ import { render } from 'react-dom';
 import App from './components/App.jsx';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import styles from './scss/application.scss';
+import axios from 'axios';
+
+const axiosInstance = axios;
 
 render(
   <App />,
