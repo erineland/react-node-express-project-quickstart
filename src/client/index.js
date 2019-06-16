d9dda0a758c656e6cc95ccb8e80078acb8c07203
arcadia168 <bennawaz168@icloud.com>
2019-06-16 17:07:37 +0100

    Adding react bootstrap and setting up

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App.jsx';
-
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import styles from './scss/application.scss';
 
 render(
